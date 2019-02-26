21e6c12814fa5f5d4f22cd1cbe3df2555c93ea04
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2019-02-25 21:01:58 -0500

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -38,7 +38,7 @@ def ffmpeg_hq():
             # idle for 30 seconds and end thread
             time.sleep(30)
             if QUEUE.empty():
-                break
+                exit(0)
 
 
 def ffmpeg():
@@ -62,7 +62,7 @@ def ffmpeg():
         else:
             time.sleep(30)
             if QUEUE.empty():
-                break
+                exit(0)
 
 
 def inmodule(filepath):
