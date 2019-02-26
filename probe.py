4d826da225b136f8e6433cd656f50005d86627ff
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2019-02-25 20:56:36 -0500

    Add files via upload

--- probe.py
+++ probe.py
@@ -11,6 +11,5 @@ def ffprobe_sync(filein: Path) -> dict:
     if not Path(filein).is_file():
         raise FileNotFoundError(f'{filein} not found')
 
-    meta = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams','-show_format',
-    str(filein)],universal_newlines=True)
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams','-show_format',str(filein)],universal_newlines=True)
     return json.loads(meta)
