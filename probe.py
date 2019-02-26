2ed705600713ec231f8b2e32973722da4117645f
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2019-02-25 20:40:21 -0500

    Add files via upload

---
+++ probe.py
@@ -0,0 +1,16 @@
+import subprocess
+import json
+from pathlib import Path
+import shutil
+if not shutil.which('ffprobe'):
+    raise FileNotFoundError('ffprobe not found')
+
+def ffprobe_sync(filein: Path) -> dict:
+    """ get media metadata """
+
+    if not Path(filein).is_file():
+        raise FileNotFoundError(f'{filein} not found')
+
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams','-show_format',
+    str(filein)],universal_newlines=True)
+    return json.loads(meta)
