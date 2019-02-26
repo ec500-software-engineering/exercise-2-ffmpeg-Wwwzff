7d660ebdfcddadb9a98560ec3d7c3d9b2b89ccf2
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2019-02-26 13:07:49 -0500

    Update test_Generate.py

--- test_Generate.py
+++ test_Generate.py
@@ -2,8 +2,6 @@ import pytest
 from ffmpeg import run, checkstatus
 import subprocess
 from probe import ffprobe_sync
-from time import sleep
-from pathlib import Path
 
 
 @pytest.fixture
@@ -17,8 +15,7 @@ def genpat(tmp_path):
 def test_duration():
     fnin = ['test.mp4']
     fnout = ['0.mp4']
-    fnout_720 = ['0_HQ.mp4']
-
+    
     orig_meta = ffprobe_sync(fnin[0])
     orig_duration = float(orig_meta['streams'][0]['duration'])
 
@@ -27,14 +24,17 @@ def test_duration():
     if checkstatus():
         meta_480 = ffprobe_sync(fnout[0])
         duration_480 = float(meta_480['streams'][0]['duration'])
-
-        meta_720 = ffprobe_sync(fnout_720[0])
-        duration_720 = float(meta_720['streams'][0]['duration'])
-
         assert orig_duration == pytest.approx(duration_480)
-        # assert orig_duration == pytest.approx(duration_720)
 
-
+        
+        
+def test_duration_720():
+    fnout_720 = ['0_HQ.mp4']
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    meta_720 = ffprobe_sync(fnout_720[0])
+    assert orig_duration == pytest.approx(duration_720)
+    
+    
 if __name__ == "__main__":
     # genpat("./")
     test_duration()
