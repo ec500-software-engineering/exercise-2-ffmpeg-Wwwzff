c3af81e41ba74102c607c5318cabe895b2af118c
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2019-02-25 20:52:37 -0500

    Add files via upload

--- ffmpeg.py
+++ ffmpeg.py
@@ -5,11 +5,11 @@ using FFMPEG to do series of jobs
 Copyright 2018 chesterw@bu.edu by Zifan Wang
 '''
 
-import os
+import subprocess
 from threading import Thread
 import time
 from queue import Queue
-import sys
+from pathlib import Path
 
 QUEUE = Queue()
 QUEUE_HQ = Queue()
@@ -29,10 +29,16 @@ def ffmpeg_hq():
         if not QUEUE_HQ.empty():
             print("[FFMPEG_HQ]Working on the "+str(COUNT_HQ+1)+" job.")
             single_file = QUEUE_HQ.get()
-            os.system("ffmpeg -i "+single_file+" -b 2M -r 30 -f mp4 -s 1280x720 -loglevel quiet "+str(COUNT_HQ)+"_HQ.mp4")
+            info = "ffmpeg -i "+single_file+" -b 2M -r 30 -f mp4 -s 1280x720 -loglevel quiet "+str(COUNT_HQ)+"_HQ.mp4"
+            subprocess.check_call(info.split(" "))
             COUNT_HQ += 1
             print("[FFMPEG_HQ]Finished "+str(COUNT_HQ + COUNT)+" now. "+str(TOTAL - COUNT - COUNT_HQ)+" left.")
             time.sleep(3)
+        else:
+            # idle for 30 seconds and end thread
+            time.sleep(30)
+            if QUEUE.empty():
+                break
 
 
 def ffmpeg():
@@ -44,13 +50,22 @@ def ffmpeg():
         if not QUEUE.empty():
             print("[FFMPEG]Working on the "+str(COUNT+1)+" job.")
             single_file = QUEUE.get()
-            os.system("ffmpeg -i "+single_file+" -b 1M -r 30 -f mp4 -s 640x480 -loglevel quiet "+str(COUNT)+".mp4")
+            info = "ffmpeg -i "+single_file+" -b 1M -r 30 -f mp4 -s hd480 -loglevel quiet "+str(COUNT)+".mp4"
+            subprocess.check_call(info.split(" "))
             COUNT += 1
             print("[FFMPEG]Finished "+str(COUNT + COUNT_HQ)+" now. "+str(TOTAL - COUNT - COUNT_HQ)+" left.")
             time.sleep(3)
+            '''
+            if not Path(str(COUNT)+".mp4").is_file():
+                raise RuntimeError(f'did not convert {single_file}')
+            '''
+        else:
+            time.sleep(30)
+            if QUEUE.empty():
+                break
 
 
-def inmodule():
+def inmodule(filepath):
     '''
     Get file path from user's input
     '''
@@ -58,40 +73,43 @@ def inmodule():
     fir_time = True
     while True:
         if QUEUE.empty() and QUEUE_HQ.empty() and TOTAL - COUNT - COUNT_HQ == 0:
-            # python ffmpeg.py ~/Videos      
-            #path = pathlib.Path(sys.argv[1])
-            #filelist = path.glob('*.mp4')
+            # python ffmpeg.py ~/Videos
+            # path = pathlib.Path(sys.argv[1])
+            # filelist = path.glob('*.mp4')
             if fir_time:
-                filepath = sys.argv[1:]
+                # filepath = sys.argv[1:]
+                # filepath ="/Users/zifanwang/Downloads/test.mp4"
                 if filepath:
                     print("[Input]Input path confirmed")
-                    TMP = filepath
-                    TOTAL += 2 * len(TMP) 
+                    TMP = list(filepath)
+                    TOTAL += 2 * len(TMP)
                     for item in TMP:
                         QUEUE.put(item)
                         QUEUE_HQ.put(item)
                     fir_time = False
-            else:
-                TMP = input("[Input]Previous work finished. Give file paths, split by space:\n")
-                if TMP:
-                    TOTAL += 2 * len(TMP) 
-                    for item in TMP:
-                        QUEUE.put(item)
-                        QUEUE_HQ.put(item)
+        else:
+            break
+            '''
+            TMP = input("[Input]Previous work finished. Give file paths, split by space:\n")
+            if TMP:
+                TOTAL += 2 * len(TMP)
+                for item in TMP:
+                    QUEUE.put(item)
+                    QUEUE_HQ.put(item)
+            '''
 
-def run():
+
+def run(filepath):
     THREADS = []
     T1 = Thread(target=ffmpeg_hq)
     THREADS.append(T1)
     T2 = Thread(target=ffmpeg)
     THREADS.append(T2)
-    T3 = Thread(target=inmodule)
-    THREADS.append(T3)
 
-    #inmodule()
+    # T3 = Thread(target=inmodule,args=filepath)
+    # THREADS.append(T3)
+
+    inmodule(filepath)
 
     for thread in THREADS:
         thread.start()
-
-if __name__ == '__main__':
-    run()
